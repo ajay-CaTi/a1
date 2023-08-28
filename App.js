ae2ac2ab166b3a5c182ad8cc173cbf1f8544358a
ajaykumarsingh110@gmail.com <ajaykumarsingh110@gmail.com>
2023-08-28 12:17:23 +0530

    nested elements from react code

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 const heading = React.createElement(
-  "h1",
-  { className: "heading" },
-  "i am heading"
+  "div",
+  { className: "parent" },
+  React.createElement(
+    "div",
+    { className: "child" },
+    React.createElement("h1", { className: "heading1" }, "Heading")
+  )
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
