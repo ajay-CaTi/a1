6c732f05a1d92b36b8dc4accc16e17078dd13bf1
ajaykumarsingh110@gmail.com <ajaykumarsingh110@gmail.com>
2023-08-28 12:20:59 +0530

    added child elements

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-const heading = React.createElement(
-  "div",
-  { className: "parent" },
+const parent = React.createElement("div", { className: "parent" }, [
   React.createElement(
     "div",
     { className: "child" },
     React.createElement("h1", { className: "heading1" }, "Heading")
-  )
-);
+  ),
+  React.createElement(
+    "div",
+    { className: "child" },
+    React.createElement("h2", { className: "heading2" }, "Heading2")
+  ),
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
